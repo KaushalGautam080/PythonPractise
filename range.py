859abaed072fde185033265e894f9ddf6a5426e2
Kaushal-VT <kaushal-vt@192.168.1.4>
2024-06-23 21:32:17 +0545

    learn about range funcion

---
+++ range.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    print(i)
+print("This is another range")
+for j in range(10, 20):
+    print(j)
+print("This is another range")
+for k in range(0, 20, 2):
+    print(k)
+
+fruits = ["apple", "banana", "mango", "grapes"]
+for i in range(len(fruits)):
+    print(i, fruits[i])
