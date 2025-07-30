b1af413f5fbfd38ebb510d0519b807015f37cd84
fastlane <fastlane@example.com>
2025-07-30 10:18:53 +0545

    feat: program to get a string made of the first 2 and last 2 characters

---
+++ day1/question2.py
@@ -0,0 +1,16 @@
+character = input("Enter string: ")
+
+new_character = ''
+
+def slice_string():
+ if len(character) < 2:
+    print("Empty String")
+ else:
+    new_character = character[:2] + character[-2:]  
+    
+ print(new_character)
+
+
+slice_string()
+
+
