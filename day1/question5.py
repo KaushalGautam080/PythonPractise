428b799c9e691e5cc0898b73507a92026244e83d
fastlane <fastlane@example.com>
2025-07-30 13:37:46 +0545

    refactor: make word unchange if length than 3

--- day1/question5.py
+++ day1/question5.py
@@ -3,7 +3,10 @@ character = input("Enter any name: ")
 
 
 def appendString(word):
-    if  word.endswith('ing'):
+    if len(word) <3:
+        word
+        
+    elif  word.endswith('ing'):
         word += 'ly'
     else:
         word += 'ing'
