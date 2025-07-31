a277f658fcce6eeb6f67f9569d00d83cd4ca246a
fastlane <fastlane@example.com>
2025-07-31 11:41:55 +0545

    feat: program to iterate over dictionaries using for loops.

---
+++ day1/question31.py
@@ -0,0 +1,11 @@
+#  Write a Python program to iterate over dictionaries using for loops. 
+sample_dict = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60} 
+
+def iterate_dict():
+    for x,y in sample_dict.items():
+        print(x,y)
+        
+iterate_dict()
+
+
+
