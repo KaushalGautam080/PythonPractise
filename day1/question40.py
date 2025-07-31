65cad4ee7540cbceec2c2444ce9877dc921d4706
fastlane <fastlane@example.com>
2025-07-31 15:49:42 +0545

    feat: crud in tuple

--- day1/question40.py
+++ day1/question40.py
@@ -1,6 +1,7 @@
 # Write a Python program to add an item in a tuple. 
 
 fruits = ("apple","banana","pear")
+print(fruits.index("pear"))
 fruits_list = list(fruits)
 fruits_list.append("mango")
 fruits = tuple(fruits_list)
