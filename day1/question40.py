77192414aad78efc398c667e5618bc13386fb8a2
fastlane <fastlane@example.com>
2025-07-31 15:12:54 +0545

    feat: program to add an item in tuple

---
+++ day1/question40.py
@@ -0,0 +1,7 @@
+# Write a Python program to add an item in a tuple. 
+
+fruits = ("apple","banana","pear")
+fruits_list = list(fruits)
+fruits_list.append("mango")
+fruits = tuple(fruits_list)
+print(fruits)
